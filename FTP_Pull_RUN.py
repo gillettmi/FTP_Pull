65923e6401b33325cc226e16071dd1bc981ad81c
Michael Gillett <51103663+gillettmi@users.noreply.github.com>
2020-04-25 23:58:02 -0400

    added username options
    added in feature that allows you to leave ftp_pull unconfigured and asks for user input when it runs. You don't have to use this if you don't want to.

--- FTP_Pull_RUN.py
+++ FTP_Pull_RUN.py
@@ -5,18 +5,14 @@
 
 # LOGIN INFORMATION ====================================================================================================
 
-# Login information and server URL /// Input your own information here
+# Login information and server URL /// Input your own information here if you don't want to have to do it every time
 username = ''
 password = ''
-ftp_url = 'ftp.website.com'
+ftp_url = ''
 
 # Remote locations to pull files from
 # if you want to pull from more than one directory, uncomment and change the section(s) below
-remote_directories = (
-    '~/directory/directory1',
-    # '~/directory1/directory2',
-    # '~/directory1/directory3',
-)
+remote_directories = []
 
 # PROGRAM SETTINGS= ====================================================================================================
 
@@ -26,9 +22,23 @@ extensions = ('.mp4', '.mpg', '.mov')
 # Set overwrite to True if you would like to overwrite files that may be incomplete downloads.
 overwrite = True
 
+# Pull only the most recent file. Set to "False" if you want to pull everything.
+pull_recent = True
+
 # RUN ==================================================================================================================
 
 # Program functions are located in ./ftp_functions.py
 if __name__ == '__main__':
+    if username == '':
+        username = input('Username:')
+    if password == '':
+        password = input('Password:')
+    if ftp_url == '':
+        ftp_url = input('FTP URL:')
+    if remote_directories:
+        pass
+    else:
+        remote_directories = input('Directories to check (Separate with commas):')
+        remote_directories = remote_directories.split(',')
     from ftp_functions import *
     main(username, password, ftp_url, remote_directories, extensions, overwrite)
